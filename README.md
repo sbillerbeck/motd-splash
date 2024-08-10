2074216bf04a4dff0cba442b998cd7d914d86612
Sam Billerbeck <sambillerbeck@gmail.com>
2024-08-10 14:30:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# splash
+This is a simple script to print a message in Linux, selected randomly from a list of string. Mostly splash texts from Minecraft.
+Users can create ".splash_off" in their home directory to disable it.
---
+++ splash.sh
@@ -0,0 +1,238 @@
+#!/bin/sh
+
+if [ ! -f "$HOME/.splash_off" ]; then
+shuf -n 1 << 'EOF' | envsubst |  xargs echo -e
+$USER IS YOU!
+"\033[34mC\033[32mo\033[32ml\033[31mo\033[31mr\033[33mm\033[37ma\033[35mt\033[35mi\033[32mc!\033[0m"
+Better than Prey!
+Alpha version!
+As seen on TV!
+Awesome!
+May contain nuts!
+More polygons!
+Moderately attractive!
+Limited edition!
+Flashing letters!
+It\'s here!
+Best in class!
+It\'s finished!
+Kind of dragon free!
+Excitement!
+More than 500 sold!
+One of a kind!
+Heaps of hits on YouTube!
+Indev!
+Spiders everywhere!
+Check it out!
+Holy cow, man!
+It\'s a game!
+Made in Sweden!
+Uses LWJGL!
+Reticulating splines!
+Minecraft!
+Yaaay!
+Singleplayer!
+Keyboard compatible!
+Ingots!
+Exploding creepers!
+That\'s no moon!
+l33t!
+Create!
+Survive!
+Dungeon!
+Exclusive!
+The bee\'s knees!
+Closed source!
+Classy!
+Wow!
+Not on Steam!
+Oh man!
+Awesome community!
+Pixels!
+Teetsuuuuoooo!
+Kaaneeeedaaaa!
+Now with difficulty!
+Enhanced!
+Pretty!
+12 herbs and spices!
+Fat free!
+Absolutely no memes!
+Free dental!
+Ask your doctor!
+Minors welcome!
+Cloud computing!
+Legal in Finland!
+Hard to label!
+Technically good!
+Bringing home the bacon!
+Indie!
+GOTY!
+Ceci n\'est pas une title screen!
+Euclidian!
+Now in 3D!
+Inspirational!
+Herregud!
+Complex cellular automata!
+Yes, sir!
+Played by cowboys!
+Now on OpenGL 3.2 core profile!
+Thousands of colors!
+Try it!
+Age of Wonders is better!
+Try the mushroom stew!
+Sensational!
+Hot tamale, hot hot tamale!
+Play him off, keyboard cat!
+Guaranteed!
+Macroscopic!
+Bring it on!
+Random splash!
+Call your mother!
+Monster infighting!
+Loved by millions!
+Ultimate edition!
+Freaky!
+You\'ve got a brand new key!
+Water proof!
+Uninflammable!
+Whoa, dude!
+All inclusive!
+Tell your friends!
+NP is not in P!
+Music by C418!
+Livestreamed!
+Haunted!
+Polynomial!
+Terrestrial!
+Full of stars!
+Scientific!
+Not as cool as Spock!
+Collaborate and listen!
+Never dig down!
+Take frequent breaks!
+Not linear!
+Han shot first!
+Nice to meet you!
+Buckets of lava!
+Ride the pig!
+Larger than Earth!
+sqrt\(-1\) love you!
+Phobos anomaly!
+Punching wood!
+Falling off cliffs!
+Synecdoche!
+Let\'s danec!
+Seecret Friday update!
+Reference implementation!
+Kiss the sky!
+20 GOTO 10!
+Verlet intregration!
+Peter Griffin!
+Do not distribute!
+Cogito ergo sum!
+4815162342 lines of code!
+A skeleton popped out!
+The sum of its parts!
+BTAF used to be good!
+I miss ADOM!
+umop-apisdn!
+OICU812!
+Bring me Ray Cokes!
+Finger-licking!
+Thematic!
+Pneumatic!
+Sublime!
+Octagonal!
+Une baguette!
+Gargamel plays it!
+Rita is the new top dog!
+SWM forever!
+Representing Edsbyn!
+Matt Damon!
+Supercalifragilisticexpialidocious!
+Consummate V\'s!
+Cow Tools!
+Double buffered!
+Hotter than the sun!
+Internet enabled!
+Autonomous!
+Engage!
+Fantasy!
+DRR!
+Kick it root down!
+Regional resources!
+Now supports åäö!
+Tip your waiter!
+Very fun!
+12345 is a bad password!
+Lives in a pineapple under the sea!
+Omnipotent!
+Gasp!
+...!
+Bees, bees, bees, bees!
+Jag känner en bot!
+This text is hard to read in a serial consooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooole!
+Hampsterdance!
+Menger sponge!
+idspispopd!
+So fresh, so clean!
+Slow acting portals!
+Try the Nether!
+Don\'t look directly at the bugs!
+Oh, ok, Pigmen!
+Finally with ladders!
+Scary!
+Play Minecraft, Watch Topgear, Get Pig!
+Jump up, jump up, and get down!
+A riddle, wrapped in a mystery!
+This parrot is no more!
+Welcome to your Doom!
+Stay a while, stay forever!
+Stay a while and listen!
+Treatment for your rash!
+"Autological" is!
+Information wants to be free!
+"Almost never" is an interesting concept!
+Lots of truthiness!
+The creeper is a spy!
+Turing complete!
+It\'s groundbreaking!
+The sky is the limit!
+Casual gaming!
+Undefeated!
+Kinda like Lemmings!
+Follow the train, CJ!
+Leveraging synergy!
+DungeonQuest is unfair!
+90210!
+Check out the far lands!
+Also try Terraria!
+Also try Mount And Blade!
+Also try Project Zomboid!
+Also try Minesweeper!
+Also try Factorio!
+Also try Rimworld!
+Also try Starsector!
+That\'s super!
+Bread is pain!
+Read more books!
+Khaaaaaaaaan!
+More addictive than lemonade!
+Bigger than a bread box!
+Millions of peaches!
+Fnord!
+This is my true form!
+Pumpkinhead!
+Has an ending!
+Finally complete!
+Feature packed!
+Stop, hammertime!
+Testificates!
+Conventional!
+Homeomorphic to a 3-sphere!
+Doesn\'t avoid double negatives!
+Does barrel rolls!
+Meeting expectations!
+Don\'t interpret contractions!
+EOF
+fi
